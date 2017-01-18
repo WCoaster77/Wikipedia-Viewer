2d2dbaa96f1c16f6cc2405c557d1b77733139796
Data Detective <datadetectiveagent007@gmail.com>
2017-01-18 11:58:34 -0800

    Add files via upload
    Initial upload

---
+++ Scripts/JavaScript.js
@@ -0,0 +1,14 @@
+﻿$.ajax( {
+	url: "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&callback=?&search=Cats",
+	//data: "Cats",
+	dataType: 'json',
+	type: 'GET',
+	headers: {'FCC Wikiviewer': 'datadetectiveagent007@gmail.com'},
+	success: function(msg) {
+    console.log(JSON.stringify(msg, null, 3));
+	}
+});
+
+
+
+//JSON.stringify(msg, null, 3)
---
+++ Scripts/_references.js
@@ -0,0 +1,5 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="bootstrap.js" />
+/// <reference path="JavaScript.js" />
+/// <reference path="jquery-3.1.1.js" />
+/// <reference path="jquery-3.1.1.slim.js" />
