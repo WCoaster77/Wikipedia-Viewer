2cf3c9d3a829688e3b6995cfc0037189af7199da
Data Detective <datadetectiveagent007@gmail.com>
2017-01-24 17:40:35 -0800

    Updated HTML and CSS

--- Scripts/JavaScript.js
+++ Scripts/JavaScript.js
@@ -1,14 +1,34 @@
-﻿$.ajax( {
-	url: "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&callback=?&search=Cats",
-	//data: "Cats",
-	dataType: 'json',
-	type: 'GET',
-	headers: {'FCC Wikiviewer': 'datadetectiveagent007@gmail.com'},
-	success: function(msg) {
+﻿var msg = '';
+
+document.getElementById('search').onkeydown = function (event) {
+    if (event.keyCode === 13) {
+        getSearch();
+    }
+};
+
+function getSearch() {
+    var data = document.getElementById('search').value;
+
+    url = "https://en.wikipedia.org";
+
+    $.ajax({
+        url: "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&callback=?&search=" + data,
+        dataType: 'json',
+        type: 'GET',
+        headers: {'FCC Wikiviewer': 'datadetectiveagent007@gmail.com'},
+        success: function (msg)
+        {
+            console.log(msg);
+        }
+    });
+}
+
+function getRandom() {
+
+}
+
+function printStr(msg) {
     console.log(JSON.stringify(msg, null, 3));
-	}
-});
-
-
+}
 
 //JSON.stringify(msg, null, 3)
